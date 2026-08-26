0c6606659890bc102ae3599f7263018d8907cc30
Pedro Henrique <phd19994@gmail.com>
2026-08-25 22:39:13 -0300

    feature: logic implementation

---
+++ scrapper/app-python/scrapper.py
@@ -0,0 +1,4 @@
+# - 1 get data from know platforms e.g sympla
+# - 2 structure and transform data into dataframe
+# - 3 create dataframe with columns event, start_date, end_date, type, location
+# - 4 generate .csv file with proper dataset
